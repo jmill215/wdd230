e492efd192af12d629505894caf9222089d5b239
jmill215 <mil23019@byui.edu>
2024-01-13 18:58:56 -0700

    changed and made code as per assignment

---
+++ scripts/getDates.js
@@ -0,0 +1,3 @@
+let currentYear = new Date().getFullYear();
+document.querySelector('#year').textContent = currentYear
+document.querySelector('#lastModified').textContent = ("Last Modification: " + document.lastModified)
