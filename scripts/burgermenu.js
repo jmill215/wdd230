3e95ea60379882f9a425b91a82ca2d882c832fbd
jmill215 <mil23019@byui.edu>
2024-01-18 23:54:25 -0700

    changed code to specify change from mobile to desktop

--- scripts/burgermenu.js
+++ scripts/burgermenu.js
@@ -2,7 +2,7 @@ const burgerButton = document.querySelector('#myButton')
 var menu = document.querySelector('.menulinks')
 
 burgerButton.addEventListener('click', () => {
-    if (burgerButton.textContent.includes ("☰")) {
+    if (burgerButton.textContent.includes ("☰") && window.innerWidth < 1000) {
         menu.style.display = 'grid';
         burgerButton.textContent = "❌";
     } else {
