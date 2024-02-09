3bf60edd0b974b5b235f24e10f526bfa21876e66
jmill215 <mil23019@byui.edu>
2024-02-09 01:57:20 -0700

    change code

--- scripts/links.js
+++ scripts/links.js
@@ -5,26 +5,29 @@ const linksURL = `https://jmill215.github.io/wdd230/data/links.json`;
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-
-    //MDN: returns an array of given object's own enumarable string-keyed property names
-    //In other words, use to easily iterate later...hopefully.
-    keys = Object.keys(data);
-    displayLinks(data, keys);
+    displayLinks(data.keys);
 }
 
-function displayLinks(data, keys) {
-    keys.forEach(key => {
-        const listItem = document.createElement("li")
-        listItem.textContent = key + ":  ";
-        data[key].forEach(link => {
-            const anchor = document.createElement("a");
-            anchor.setAttribute("href", link.link);
-            anchor.setAttribute("target", "_blank")
-            anchor.textContent(link.name + " | ")
-            listItem.appendChild(anchor);
-        });
-        activities.appendChild(listItem);
-    })
-}
+//Clearing data here as I have not managed to figure out a method for display...despite having done the other activity.
 
-getLinks();
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+    // Build the image portrait by setting all the relevant attributes
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+        card.appendChild(fullName); //fill in the blank
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+  }
