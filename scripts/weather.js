deaff6b23fbc4c163133c2fc643607e9806c7ce4
jmill215 <mil23019@byui.edu>
2024-02-09 01:25:24 -0700

    changed misspellings in code

--- scripts/weather.js
+++ scripts/weather.js
@@ -22,7 +22,7 @@ apiFetch();
 
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    Temp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
