86bcb2aed44bcb3fbc83da3438b9f8edf9650a48
jmill215 <mil23019@byui.edu>
2024-02-08 21:38:54 -0700

    changed base URL

--- scripts/links.js
+++ scripts/links.js
@@ -3,26 +3,46 @@ const Activities = document.querySelector("#activity-display")
 const LinksURL = "https://jmill215.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(LinksURL);
+    const response = await fetch(JsonLink);
     const data = await response.json();
-    //console.log(data);
-    keys = Object.keys(data)
-    displayLinks(data, keys);
+    displayLinks(data.weeks)
 }
 
-function displayLinks(data, keys) {
-    keys.forEach(key => {
-        const li = document.createElement("li")
-        li.textContent = key + ": "
-        data[key].forEach(link => {
-            const anchor = document.createElement("a")
-            anchor.setAttribute("href", link.link);
-            anchor.setAttribute("target", "_blank");
-            anchor.textContent = link.name + " | "
-            li.appendChild(anchor)
-        })
-        Activities.appendChild(li);
+const displayLinks = (weeks) => {
+    weeks.foreach((week) => {
+    let weekNum = document.createElement("li");
+
+    weekNum.textContent = `${week.week}:  `;
+    Activities.appendChild(weekNum);
+
+    week.links.foreach((link) => 
+    {
+        let anchor = document.createElement("a");
+
+        anchor.textContent = link.title;
+        anchor.setAttribute("href", baseURL + link.url);
+
+        weekNum.appendChild(anchor);
     })
+});
+
 }
+
+
+
+//function displayLinks(data,keys) {
+//    keys.forEach(key => {
+//        const li = document.createElement("li");
+//        li.textContent = key + ": ";
+//        data[key].forEach(link => {
+ //           const a = document.createElement("a");
+//            a.setAttribute("href", link.link);
+//            a.setAttribute("target", "_blank");
+//            a.textContent = link.name + " | ";
+//            li.appendChild(a);
+ //       });
+//        ActivitiesElement.appendChild(li);
+//    });
+//}
 
 getLinks();
